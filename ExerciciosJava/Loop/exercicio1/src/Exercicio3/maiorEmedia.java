6d53785410247b6a983a1b1af47d92910169e99a
PAFreire <pa_pauloandre@yahoo.com.br>
2023-03-09 20:04:08 -0300

    final tarefa media

--- ExerciciosJava/Loop/exercicio1/src/Exercicio3/maiorEmedia.java
+++ ExerciciosJava/Loop/exercicio1/src/Exercicio3/maiorEmedia.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 
-public class maiorEmedia(){
-    public static void main (String[] args)throws Exception {
-    
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+public class maiorEmedia{
+    public static void main (String[] args) throws Exception {   
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        int maior = 0;
+        int count = 0;
+        int soma = 0;
 
-                int numero;
+        do{
+            System.out.println("Digite o numero: ");
+            numero = sc.nextInt();
 
-                System.out.println("Digite o numero: ");
-                numero = sc.nextInt();
+            soma = soma + numero;
+            System.out.println("A soma é :" + soma);
 
-
-                System.out.println("O maior numero "+numero);
+            if(numero > maior) maior = numero;
             
-           
+            count = count + 1;
+
+        }while(count < 5);
+
+        System.out.println("Qual o maior numero? "+maior);
+        System.out.println("A média é: " + (soma / 5));
         }
     }
 
-}
+
 
