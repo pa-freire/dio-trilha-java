7395f8a7b0e5e93853cd17c604f70cfa823babdc
Paulo André Freire <99456729+pa-freire@users.noreply.github.com>
2023-03-09 11:47:33 -0300

    Add files via upload

---
+++ ExerciciosJava/Loop/exercicio1/src/Exercicio1/Exe1nomeIdade.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exe1nomeIdade {
+    /**
+     * 
+     */
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in); {
+        
+            String nome;
+            int idade;
+
+        while(true){
+                System.out.println("Nome ");
+                nome = scan.next();
+                if (nome.equals("0")) break;
+   
+                System.out.println("Idade ");
+                idade = scan.nextInt();
+                }
+        }
+        System.out.println("Continua aqui....");
+    }
+}
---
+++ ExerciciosJava/Loop/exercicio1/src/Exercicio3/maiorEmedia.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class maiorEmedia(){
+    public static void main (String[] args)throws Exception {
+    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+                int numero;
+
+                System.out.println("Digite o numero: ");
+                numero = sc.nextInt();
+
+
+                System.out.println("O maior numero "+numero);
+            
+           
+        }
+    }
+
+}
+
